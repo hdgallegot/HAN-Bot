eea087d46522bdad6392030d427022198cc053d1
aldajo92 <alejodiscovery20@gmail.com>
2014-08-31 22:26:17 +0000

    addig simple example motor

---
+++ node/testHanBot/singlemotoroperatio.js
@@ -0,0 +1,88 @@
+var b = require('bonescript');
+
+var in1 = 'P8_9';
+var in2 = 'P8_10';
+var in3 = 'P8_15';
+var in4 = 'P8_16';
+var enA = 'P8_13';
+var enB = 'P8_19';
+
+b.pinMode(in1, b.OUTPUT);
+b.pinMode(in2, b.OUTPUT);
+b.pinMode(in3, b.OUTPUT);
+b.pinMode(in4, b.OUTPUT);
+
+var state = 0;
+
+//setInterval(operation, 1000);
+
+function operation(){
+	state = state ? 0 : 1;
+
+	b.digitalWrite(in1, state);
+	b.digitalWrite(in2, state);
+	b.digitalWrite(in3, state);
+	b.digitalWrite(in4, state);
+}
+
+switch (Number(process.argv[2])){
+	case 0:
+		moveForward(0);
+		break;
+	case 1:
+		moveForward(Number(process.argv[3]));
+		break;
+	case 2:
+		moveBack(Number(process.argv[3]));
+		break;
+	case 3:
+		moveUnclock(Number(process.argv[3]));
+		break;
+	case 4:
+		moveClock(Number(process.argv[3]));
+		break;
+}
+
+//moveForward(Number(process.argv[2]));
+
+function moveForward(value){
+	b.digitalWrite(in1, b.LOW);
+	b.digitalWrite(in2, b.HIGH);
+	b.analogWrite(enA, value);
+	b.digitalWrite(in3, b.LOW);
+	b.digitalWrite(in4, b.HIGH);
+	b.analogWrite(enB, value);
+}
+
+function moveBack(value){
+	b.digitalWrite(in1, b.HIGH);
+	b.digitalWrite(in2, b.LOW);
+	b.analogWrite(enA, value);
+	b.digitalWrite(in3, b.HIGH);
+	b.digitalWrite(in4, b.LOW);
+	b.analogWrite(enB, value);
+}
+
+function moveClock(value){
+	b.digitalWrite(in1, b.LOW);
+	b.digitalWrite(in2, b.HIGH);
+	b.analogWrite(enA, value);
+	b.digitalWrite(in3, b.HIGH);
+	b.digitalWrite(in4, b.LOW);
+	b.analogWrite(enB, value);
+}
+
+function moveUnclock(value){
+	b.digitalWrite(in1, b.HIGH);
+	b.digitalWrite(in2, b.LOW);
+	b.analogWrite(enA, value);
+	b.digitalWrite(in3, b.LOW);
+	b.digitalWrite(in4, b.HIGH);
+	b.analogWrite(enB, value);
+}
+
+function compareData(data){
+	if(data == "hola") {
+		console.log('correcto');
+	}
+}
